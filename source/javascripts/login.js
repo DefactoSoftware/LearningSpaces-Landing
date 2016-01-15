846a82809da23a4db0ad83dc723c8c34f2d6b056
jurre <jurrestender+github@gmail.com>
2016-01-15 12:40:30 +0100

    Port over login page

---
+++ source/javascripts/login.js
@@ -0,0 +1,17 @@
+var tld = window.location.hostname.split('.').pop();
+
+$(function () {
+  $('#tld').text(".learningspaces." + tld);
+  var onClickHandler = function () {
+    var subdomain = $('#subdomain').val().toLowerCase();
+    var endpoint = "https://public.learningspaces." + tld + "/api/v1/accounts/" + subdomain;
+    $.ajax(endpoint).fail(function () {
+      $("#error").show();
+    }).done(function () {
+      var url = 'https://' + subdomain + '.learningspaces.' + tld + '/login';
+      window.location = url;
+    });
+  };
+
+  $('#continue').on('click', onClickHandler);
+});
